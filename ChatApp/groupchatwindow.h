cb381cfb0e17909f59f5b6e6cb790ebcba7d424e
aabdurrahm129 <aabdurrahm129@aucegypt.edu>
2026-04-28 15:51:13 +0300

    Fix GroupChatWindow header and UI

--- ChatApp/groupchatwindow.h
+++ ChatApp/groupchatwindow.h
@@ -2,6 +2,7 @@
 #define GROUPCHATWINDOW_H
 
 #include <QWidget>
+#include "INetworkClient.h"
 
 namespace Ui {
 class GroupChatWindow;
@@ -12,11 +13,17 @@ class GroupChatWindow : public QWidget
     Q_OBJECT
 
 public:
-    explicit GroupChatWindow(QWidget *parent = nullptr);
+    explicit GroupChatWindow(QWidget *parent = nullptr, INetworkClient* network = nullptr, QString groupName = "");
     ~GroupChatWindow();
+
+private slots:
+    void on_pushButton_Send_clicked();
+    void on_pushButton_Back_clicked();
 
 private:
     Ui::GroupChatWindow *ui;
+    INetworkClient* network;
+    QString groupName;
 };
 
 #endif // GROUPCHATWINDOW_H
