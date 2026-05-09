55577a86eebe283bdfab2c6ef508036783520b87
aabdurrahm129 <aabdurrahm129@aucegypt.edu>
2026-05-09 19:07:13 +0300

    final fixes done

--- ChatApp/main.cpp
+++ ChatApp/main.cpp
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
 
     QObject::connect(&loginWindow, &LoginWindow::loginSuccessful, [&](const QString& username){
         loginWindow.hide();
+        userListWindow.initGroupManager();
         userListWindow.show();
         QTimer::singleShot(500, [&](){
             networkClient.requestUserList();
